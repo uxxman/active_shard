28a64213f2ef1a2bb5dab71dbde75e4d53f9fe0e
Muhammad Usman <uxman.sherwani@gmail.com>
2023-08-30 07:52:07 +0200

    Adding guid method to all Sharded records

--- lib/active_shard/record.rb
+++ lib/active_shard/record.rb
@@ -3,5 +3,9 @@ module ActiveShard
     self.abstract_class = true
 
     connects_to shards: Rails.application.config_for(:shards)
+
+    def guid
+      [Bank.current_id, id].compact.join('.')
+    end
   end
 end
